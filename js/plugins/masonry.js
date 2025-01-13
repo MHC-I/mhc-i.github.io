33815b065d59f58a658ef08327cbb2975da03849
🐵 Co <coco@Entecavirs-Air.rove-saury.ts.net lan>
2025-01-13 22:46:50 +0800

    Site updated: 2025-01-13 22:46:50

--- js/plugins/masonry.js
+++ js/plugins/masonry.js
@@ -1,1 +1,57 @@
-function initMasonry(){var t,e,n,r,o=document.querySelector("#masonry-container");o&&(t=document.querySelectorAll("#masonry-container .masonry-item img[data-src]"),e=768<=window.innerWidth?255:150,n=new MiniMasonry({baseWidth:e,container:o,gutterX:10,gutterY:10,surroundingGutter:!1,lazyLoad:!0,fitWidth:!0}),r=new IntersectionObserver(t=>{t.forEach(e=>{if(e.isIntersecting){let t=e.target;t.src=t.getAttribute("data-src"),t.onload=()=>{t.style.height="",n.layout()},r.unobserve(t)}})}),t.forEach(t=>{r.observe(t)}))}if(data.masonry){try{swup.hooks.on("page:view",initMasonry)}catch(t){console.error("Error setting up swup hooks:",t)}document.addEventListener("DOMContentLoaded",initMasonry)}export{initMasonry};
+export function initMasonry() {
+  var masonryContainer = document.querySelector("#masonry-container");
+  if (!masonryContainer) return;
+
+  var images = document.querySelectorAll(
+    "#masonry-container .masonry-item img[data-src]"
+  );
+
+  // 瀑布流初始化
+  var screenWidth = window.innerWidth;
+  var baseWidth;
+  if (screenWidth >= 768) {
+    baseWidth = 255;
+  } else {
+    baseWidth = 150;
+  }
+
+  var masonry = new MiniMasonry({ 
+    baseWidth: baseWidth,
+    container: masonryContainer,
+    gutterX: 10,
+    gutterY: 10,
+    surroundingGutter: false,
+    lazyLoad: true,
+    fitWidth: true, 
+  });
+
+  // 懒加载同时刷新瀑布流布局
+  var observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        img.src = img.getAttribute('data-src');
+        img.onload = () => {
+          img.style.height = '';
+          masonry.layout(); // 图片加载完成后更新布局
+        };
+        // 处理加载失败的情况
+        observer.unobserve(img);
+      }
+    });
+  });
+
+  images.forEach(img => {
+    observer.observe(img);
+  });
+}
+
+
+if (data.masonry) {
+  try {
+    swup.hooks.on("page:view", initMasonry);
+  } catch (e) {
+    console.error("Error setting up swup hooks:", e);
+  }
+  document.addEventListener("DOMContentLoaded", initMasonry);
+}
